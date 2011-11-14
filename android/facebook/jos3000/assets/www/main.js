8d32f57686650b3ffd49f7d1926b88a98670733d
John Tubert <jtubert@hotmail.com>
2011-11-14 10:04:18 -0500

    facebook test

---
+++ android/facebook/jos3000/assets/www/main.js
@@ -0,0 +1,29 @@
+appId = 285414425069 // this is your facebook app id
+
+
+
+
+function logout() {
+	window.plugins.facebook.logout(function(res) {
+		alert(res);
+	});
+}
+            
+function login() {
+    window.plugins.facebook.authorize(appId, function(res) {
+	    if(res.token !== undefined) {
+	        window.plugins.facebook.request("me" ,function(res){
+		        alert(res.name); 
+		    });
+	    } else {
+	        // we have to call authorize
+	        window.plugins.facebook.getAccess(function(res) {
+	            if(res.token !== undefined) {
+	                window.plugins.facebook.request("me" ,function(res){
+				        alert(res.name); 
+				    });
+	            }
+	        });
+	    }
+	});
+}
